c0b7816865ddd253b13ac90fc392413b3941c870
Jeremy Cole <jeremy@jcole.us>
2024-11-01 15:24:00 -0700

    Use let instead of @-variable

--- spec/intel_hex/file_reader/blink_spec.rb
+++ spec/intel_hex/file_reader/blink_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "blink.hex" do
-  before :all do
-    @records = [
+  let(:expected_records) do
+    [
       { type: :data, offset: 0x0000, length: 0x10, checksum: 0x82 },
       { type: :data, offset: 0x0010, length: 0x10, checksum: 0x68 },
       { type: :data, offset: 0x0020, length: 0x10, checksum: 0x58 },
@@ -12,7 +12,7 @@ RSpec.describe "blink.hex" do
       { type: :data, offset: 0x0080, length: 0x10, checksum: 0x8c },
       { type: :data, offset: 0x0090, length: 0x10, checksum: 0x63 },
       { type: :data, offset: 0x00a0, length: 0x10, checksum: 0x14 },
-      { type: :eof,  offset: 0x0000, length: 0x00, checksum: 0xff },
+      { type: :eof, offset: 0x0000, length: 0x00, checksum: 0xff },
     ].freeze
   end
 
@@ -21,13 +21,13 @@ RSpec.describe "blink.hex" do
     intel_hex_file = IntelHex::FileReader.new(blink_hex)
     records = intel_hex_file.each_record.to_a
 
-    expect(records.size).to eq @records.size
-    @records.each_with_index do |expected_record, i|
+    expect(records.size).to eq expected_records.size
+    expected_records.each_with_index do |expected_record, i|
       expect(records[i].type).to      eq expected_record[:type]
       expect(records[i].offset).to    eq expected_record[:offset]
       expect(records[i].length).to    eq expected_record[:length]
       expect(records[i].checksum).to  eq expected_record[:checksum]
-      expect(records[i].valid?).to    eq true
+      expect(records[i].valid?).to    be true
       expect(records[i].to_ascii).to  eq records[i].line
     end
   end
